9c3f9698ea42729ffa4437ead993de72cca027f6
rAnnabell <annabell.roessler@outlook.com>
2023-06-21 22:10:58 +0200

    finished documentation of TextureObjectNode.js

--- code/src/creations/TextureObjectNode.js
+++ code/src/creations/TextureObjectNode.js
@@ -2,11 +2,21 @@
  * Extends AdvancedTextureSGNode in order to set the uniform u_enableObjectTexture only if an object actually uses a texture.
  */
 class TexturedObjectNode extends AdvancedTextureSGNode {
+
+    /**
+     * Constructor for class TexturedObjectNode.
+     * @param image     The image used as a texture.
+     * @param children  The children list.
+     */
     constructor(image, children) {
         children.push(new SetUniformSGNode("u_enableObjectTexture", true));
         super(image, children);
     }
 
+    /**
+     * Render method to render this node.
+     * @param context   The context.
+     */
     render(context) {
         gl.uniform1i(gl.getUniformLocation(context.shader, 'u_enableObjectTexture'), 1);
         super.render(context);
